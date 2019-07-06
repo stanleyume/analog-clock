f2a0622f7c51aa8cb374319ef4f37b0f2662340d
Stanley <coolstanlee@gmail.com>
2019-07-06 11:24:43 +0100

    AM / PM

--- clock.js
+++ clock.js
@@ -5,13 +5,17 @@ function clock (timezone) {
   let seconds = now.getSeconds();
   let minute = now.getMinutes();
   let hour = now.getHours();
+  let ampm = (hour >= 12) ? "P.M" : "A.M";
 
   // For real hour hand movement
   hour = hour + (minute/60);
+  // For real minute hand movement
+  minute = minute + (seconds/60);
   
   document.querySelector('.hand.second').style.transform = `rotate(${seconds * 6 + 90}deg)`;
   document.querySelector('.hand.minute').style.transform = `rotate(${minute * 6 + 90}deg)`;
   document.querySelector('.hand.hour').style.transform = `rotate(${hour * 30 + 90}deg)`;
+  document.querySelector('.ampm').textContent = ampm;
 
   audio.currentTime = 0;
   audio.play().catch(error => {});
